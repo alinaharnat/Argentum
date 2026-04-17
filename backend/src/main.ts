f0fb5e4ad224a3558954cf415133739f0c3bca71
Alina Harnat <alinasharnat@gmail.com>
2026-04-17 22:41:00 +0300

    feat: setup base backend structure

---
+++ backend/src/main.ts
@@ -0,0 +1,31 @@
+import { NestFactory, Reflector } from "@nestjs/core";
+import { ValidationPipe, ClassSerializerInterceptor } from "@nestjs/common";
+import cookieParser from "cookie-parser";
+import { AppModule } from "./modules/app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const envService = app.get("EnvService");
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: envService.frontendUrl,
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(envService.port);
+}
+bootstrap();
---
+++ backend/src/modules/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from "@nestjs/common";
+import { EnvModule } from "./env/env.module";
+import { DatabaseModule } from "./database/database.module";
+
+@Module({
+  imports: [EnvModule, DatabaseModule],
+})
+export class AppModule {}
