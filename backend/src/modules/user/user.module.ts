10d21eb4acb90f12f1a4ed7ffa4a38c858113d02
Alina Harnat <alinasharnat@gmail.com>
2026-04-19 00:57:15 +0300

    feat: created user module

---
+++ backend/src/modules/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./schemas";
+import { UserRepository } from "./repositories";
+import { UserService } from "./services";
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+
+  providers: [UserService, UserRepository],
+  exports: [UserService],
+})
+export class UsersModule {}
