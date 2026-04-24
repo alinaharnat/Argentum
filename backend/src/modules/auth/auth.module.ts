a484d3d45bfbfa9576f881e81c94683a0212ea01
Alina Harnat <alinasharnat@gmail.com>
2026-04-25 00:36:18 +0300

    feat: created auth module

---
+++ backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { UserModule } from "../user/user.module";
+import { AuthService } from "./services";
+import { TokenStoreService } from "./services";
+import { TokenGenerationService } from "./services";
+import { SessionService } from "./services";
+import { AuthController } from "./controllers";
+import { RefreshTokenRepository } from "./repository";
+import { RefreshToken, RefreshTokenSchema } from "./schemas";
+import { CommonModule } from "../common/common.module";
+import { JwtModule } from "@nestjs/jwt";
+import { MongooseModule } from "@nestjs/mongoose/dist/mongoose.module";
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: RefreshToken.name, schema: RefreshTokenSchema },
+    ]),
+    JwtModule.register({}),
+    UserModule,
+    CommonModule,
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    TokenGenerationService,
+    TokenStoreService,
+    SessionService,
+    RefreshTokenRepository,
+  ],
+})
+export class AuthModule {}
