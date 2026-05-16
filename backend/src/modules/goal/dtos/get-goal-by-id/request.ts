41a0b771d3b118263f67b290439e7a887414f8bc
Alina <aharnat@gmail.com>
2026-05-16 22:58:24 +0300

    refactor(goal): remove MongoDB validation from GetGoalByIdRequestDto

--- backend/src/modules/goal/dtos/get-goal-by-id/request.ts
+++ backend/src/modules/goal/dtos/get-goal-by-id/request.ts
@@ -2,6 +2,5 @@ import { IsMongoId, IsString } from "class-validator";
 
 export class GetGoalByIdRequestDto {
   @IsString()
-  @IsMongoId({ message: "Goal id must be a valid MongoDB ObjectId" })
   id: string;
 }
